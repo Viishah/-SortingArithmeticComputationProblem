a6cd23e2aa9afd97f2cc8e68aa582fd6bc23e1ce
“Viishah” <vtorsalle@gmail.com>
2023-03-06 12:26:47 +0530

    Add : UC3-compute

--- sortingArithmeticComputationProblem.sh
+++ sortingArithmeticComputationProblem.sh
@@ -2,6 +2,6 @@ echo "Welcome to the Sorting Arithmetic"
 read -p "Enter a first number: " a
 read -p "Enter b first number: " b
 read -p "Enter c first number: " c
-calculate=$(( $a + $b * $c))
+calculate=$(( $a * $b + $c))
 echo "calculate a + b * c:-"$calculate
 
