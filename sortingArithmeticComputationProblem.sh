87919cf8ad3d82eaf3180f740b46eef7d789fc57
“Viishah” <vtorsalle@gmail.com>
2023-03-06 12:16:01 +0530

    Add : Sort the results to show the Computation Valuein Ascending Order

---
+++ sortingArithmeticComputationProblem.sh
@@ -0,0 +1,27 @@
+echo "Welcome to the Sorting Arithmetic"
+read -p "Enter a first number: " a
+read -p "Enter b first number: " b
+read -p "Enter c first number: " c
+
+temp1=$(($a + $b * $c))
+temp2=$((a * b + c))
+temp3=$((c + a / b))
+temp4=$((a % b + c))
+
+declare -A value
+
+value[1]="$temp1"
+value[2]="$temp2"
+value[3]="$temp3"
+value[4]="$temp4"
+
+counter=${value[@]}
+
+temp=($counter)
+variable=${temp[*]}
+echo $variable
+
+for i in $( echo "$variable" | tr ' ' '\n' | sort -n )
+do
+  echo $i
+done
