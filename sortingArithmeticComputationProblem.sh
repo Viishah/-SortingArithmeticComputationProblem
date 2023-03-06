5bc654b32c49db92473253e43879a60608f434b2
“Viishah” <vtorsalle@gmail.com>
2023-03-06 12:21:48 +0530

    Add : UC2-compute

--- sortingArithmeticComputationProblem.sh
+++ sortingArithmeticComputationProblem.sh
@@ -2,26 +2,6 @@ echo "Welcome to the Sorting Arithmetic"
 read -p "Enter a first number: " a
 read -p "Enter b first number: " b
 read -p "Enter c first number: " c
+calculate=$(( $a + $b * $c))
+echo "calculate a + b * c:-"$calculate
 
-temp1=$(($a + $b * $c))
-temp2=$((a * b + c))
-temp3=$((c + a / b))
-temp4=$((a % b + c))
-
-declare -A value
-
-value[1]="$temp1"
-value[2]="$temp2"
-value[3]="$temp3"
-value[4]="$temp4"
-
-counter=${value[@]}
-
-temp=($counter)
-variable=${temp[*]}
-echo $variable
-
-for i in $( echo "$variable" | tr ' ' '\n' | sort -n )
-do
-  echo $i
-done
